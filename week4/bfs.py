5e0cac0f5cdef73d2b0abd3c8c02012344c01a69
BSanandu88 <bsanandu88@gmail.com>
2021-11-28 11:17:58 +0530

    bfs

--- week4/bfs.py
+++ week4/bfs.py
@@ -59,3 +59,42 @@ def BFSList(AList,v):   # Adjacency list
                 q.addq(k)
 
     return(visited)
+
+
+# Path in bfs
+def BFSListPath(AList,v):
+    (visited,parents) = ({},{})
+    for i in AList.keys():
+        visited[i] = False
+        parents[i] = -1
+    q = Queue()
+    visited[v] = True
+    q.addq(v)
+    while(not q.isempty()):
+        j = q.delq()
+        for k in AList[j]:
+            if(not visited[k]):
+                visited[k] = True
+                parents[k] = j
+                q.addq(k)
+    return(visited,parents)
+
+
+def BFSListPathLevel(Alist,v):
+    (level,parent) = ({},{})
+    for i in Alist.keys():
+        level[i] = -1
+        parent[i] = -1
+    q = Queue()
+    level[v] = 0
+    q.addq(v)
+    while(not q.isempty()):
+        j = q.delq()
+        for k in Alist[j]:
+            if(level[k] == -1):
+                level[k] = level[j] + 1
+                parent[k] = j
+                q.addq(k)
+    return(level,parent)
+
+
