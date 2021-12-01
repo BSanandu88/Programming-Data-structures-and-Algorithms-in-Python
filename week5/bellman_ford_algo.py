e5b1f42e6aec8b14d6d2df24be03aa056c360204
BSanandu88 <bsanandu88@gmail.com>
2021-12-01 18:55:34 +0530

    bellman ford

--- week5/bellman_ford_algo.py
+++ week5/bellman_ford_algo.py
@@ -15,3 +15,19 @@ def bellmanford(WMat,s):
                 if WMat[u,v,0] == 1:
                     distance[v] = min(distance[v],distance[u] + WMat[u,v,1] )
     return(distance)
+
+
+# using adjaceny list
+
+def bellmanfordlist(WList,s):
+    infinity = 1 + len(WList.keys()) * max([d for u in WList.keys()
+                                             for (v,d) in WList[u]])
+    distance = {}
+    for v in WList.keys():
+        distance[v] = infinity
+    distance[s] = 0
+    for i in WList.keys():
+        for u in WList.keys():
+            for (v,d) in WList[u]:
+                distance[v] = min(distance[v],distance[u] + d)
+    return(distance)
