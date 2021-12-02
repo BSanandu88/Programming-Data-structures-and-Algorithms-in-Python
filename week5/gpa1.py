0e3d48332ad3ceeb606e1131db2f9f6ad2257347
BSanandu88 <bsanandu88@gmail.com>
2021-12-02 20:42:55 +0530

    gpa

--- week5/gpa1.py
+++ week5/gpa1.py
@@ -1,5 +1,6 @@
 
 # 1 METHOD
+from abc import abstractproperty
 from typing import TYPE_CHECKING
 
 
@@ -41,3 +42,28 @@ for ed in edges:
     WL[ed[1]].append((ed[0],ed[2]))
 print(FiberLink(WL))
 
+#   2nd method
+
+def kruskal(WList):
+    (edges,component,TE) = ([],{},[])
+    for u in WList.keys():
+        edges.extend([(d,u,v) for (v,d) in WList[u]])
+        component[u] = u
+    edges.sort()
+    for (d,u,v) in edges:
+        if component[u] != component[v]:
+            TE.append((u,v))
+            c = component[u]
+        for w in WList.keys():
+            if component[w] == c:
+                component[w] = component[v]
+    return(TE)
+
+def FiberLink(distance_map):
+    R = kruskal(distance_map)
+    s = 0
+    for e in R:
+        for ed in distance_map[e[0]]:
+            if ed[0] == e[1]:
+                s += ed[1]
+    return s
