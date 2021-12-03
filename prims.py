26e49111dfee54230512a0d7274362f4a6903c41
BSanandu88 <bsanandu88@gmail.com>
2021-12-03 17:53:07 +0530

    prims

--- prims.py
+++ prims.py
@@ -20,3 +20,24 @@ def primlist(Wlist):
             if not visited[v]:
                 distance[v] = min(distance[v],d)
     return(treeEdges)
+
+# improved version
+def primlist2(Wlist):
+    infinity = 1 + max([d for u in Wlist.keys() for (v,d) in Wlist[u]])
+    (visited,distance,nbr) = ({},{},{})
+    for v in Wlist.keys():
+        (visited[v],distance[v],nbr[v]) = (False,infinity,-1)
+    visited[0] = True
+    for (v,d) in Wlist[0]:
+        (distance[v],nbr[v]) = (d,0)
+    for i in range(1,len(Wlist.keys())):
+        nextd = min([distance[v] for v in Wlist.keys() if not visited[v]])
+        nextvlist = [v for v in Wlist.keys() if (not visited[v]) and distance[v] == nextd]
+        if nextvlist == []:
+            break
+        nextv = min(nextvlist)
+        visited[nextv] = True
+        for (v,d) in Wlist[nextv]:
+            if not visited[v]:
+                (distance[v],nbr[v]) = (min(distance[v],d),nextv)
+    return(nbr)
