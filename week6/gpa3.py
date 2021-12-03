0445a61e2700cb304adee3435347cc370d05d0b5
BSanandu88 <bsanandu88@gmail.com>
2021-12-03 21:11:37 +0530

    gpa 3

---
+++ week6/gpa3.py
@@ -0,0 +1,19 @@
+
+
+def heapify(A,n,i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and A[largest] < A[l]:
+        largest = i
+    if r < n and A[largest] < A[r]:
+        largest = r
+    if largest != i:
+        A[i],A[largest] = A[largest],A[i]
+        heapify(A,n,largest)
+
+
+def min_max(A):
+    n = len(A)
+    for i in range(n // 2,-1,-1):
+        heapify(A,n,i)
